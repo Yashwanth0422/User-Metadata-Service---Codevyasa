43fa649293997447f78069e6c203b7c94c28cc20
Yashwanth0422 <yashuyashwanth95@gmail.com>
2026-04-18 10:47:42 +0530

    all the changes done in the fork

--- app/circuit.py
+++
@@ -1,6 +0,0 @@
-import pybreaker
-
-db_breaker = pybreaker.CircuitBreaker(
-    fail_max=5,
-    reset_timeout=10
-)
--- app/db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="usersdb",
-        user="user",
-        password="password"
-    )
